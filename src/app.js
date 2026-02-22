c056e2eaa23b3ac2f9addb962c63b1887aa46bc7
tavish-crypto <tavish.modi.s.125@kalvium.community>
2026-02-22 11:46:37 +0530

    Initial commit

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use('/api/artist', require('../src/routes/artist.route'));
 app.use('/api/album', require("../src/routes/album.route"));
 
 app.get("/", (req, res) => {
-    res.send("Hello, World!");
+    res.send("Hello, TAVISH!!  WUDUUU??");
 });
 
 module.exports = app;
